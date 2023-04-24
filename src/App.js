9ede8d52a9f85033792b4c5b3b1c2a855756f3fe
classehazel <classehazel@gmail.com>
2023-04-24 17:57:12 -0500

    solving routing issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import AboutUs from "./pages/AboutUs";
